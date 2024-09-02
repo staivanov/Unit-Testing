c126bbd4cd8e08d92b74e8956d324e306b5ee48d
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-02 08:07:20 +0300

    Tests with SetUp method for code reuse.

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,18 +6,46 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        //SetUp
+        //TearDown
+
+        private Math _math;
+
+        public void SetUp()
+            => _math = new Math();
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            Math math = new Math();
-            int result = math.Add(1, 2);
+            int result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
 
 
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            int result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
 
 
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            int result = _math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
 
+        }
+
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            int result = _math.Max(2, 2);
+            Assert.That(result, Is.EqualTo(2));
+
+        }
     }
 }
