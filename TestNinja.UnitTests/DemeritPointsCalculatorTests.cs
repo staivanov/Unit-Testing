0e16d01f1b639581eeda68961ea787e9271dc353
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-04 08:09:27 +0300

    Test class for DemeritPointsCalculator was created.

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-100)]
+        [TestCase(600)]
+        public void CalculateDemeritPoints_IsSpeedPossitiveOrSpeedExceedMaxSpeed_ThrowNewArgumentOutOfRangeException(int speed)
+        {
+            DemeritPointsCalculator calculator = new DemeritPointsCalculator();
+            Assert.Throws<ArgumentOutOfRangeException>(() => calculator.CalculateDemeritPoints(speed));
+        }
+
+
+        [Test]
+        [TestCase(65)]
+        [TestCase(30)]
+        public void CalculateDemeritPoints_SpeedIsBelowOrEqualSpeedLimit_ReturnZero(int speed)
+        {
+            DemeritPointsCalculator calculator = new DemeritPointsCalculator();
+            int result = calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(30)]
+        public void CalculateDemeritPoints_GetDemeritPoints_ReturnDemeritPoints(int speed)
+        {
+            DemeritPointsCalculator calculator = new DemeritPointsCalculator();
+            int result = calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.Not.Zero);
+        }
+    }
+}
