8634f474dd0ac89f460d40912a443cf2be7c57c5
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-04 12:32:14 +0300

    Testing interaction between two objects.

---
+++ TestNinja.UnitTests/OrderService/OrderServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {   //Arrange
+            Mock<IStorage> storage = new Mock<IStorage>();
+            OrderService service = new OrderService(storage.Object);
+            //Act
+            Order newOrder = new Order();
+            service.PlaceOrder(newOrder);
+            //Assert
+            storage.Verify(s => s.Store(newOrder));
+        }
+
+
+
+
+
+
+
+    }
+}
