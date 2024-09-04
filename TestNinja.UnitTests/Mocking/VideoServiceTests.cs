586db4682fdd627b5eaa7951fd116e738153bf37
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-04 10:39:11 +0300

    DI via constructor injection.

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
+        {
+            VideoService videoService = new VideoService(new FakeFileReader());       
+            string result = videoService.ReadVideoTitle(),
+                errorMessage = "error";
+
+            Assert.That(result, Does.Contain(errorMessage).IgnoreCase);
+        }
+
+
+
+
+
+
+    }
+}
