a0d357bd47d7389631c0c3f08deeabe5671599a1
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-09-04 09:21:08 +0300

    Video service class was refactored to become loosely coupled.

---
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return string.Empty;
+        }
+    }
+}
