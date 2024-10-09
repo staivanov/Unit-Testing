fae497e0c8c6e109354b58fdee75c8872b3943d1
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-10-09 09:50:17 +0300

    Test for pop method were implemented.

--- TestNinja.UnitTests/Stack/StackTests.cs
+++ TestNinja.UnitTests/Stack/StackTests.cs
@@ -43,6 +43,48 @@ namespace TestNinja.UnitTests.Stack
 
 
 
+        [Test]
+        public void Pop_StackWithObjects_ReturnObjOnTheTop()
+        {
+            Stack<int> myStack = new Stack<int>();
+            myStack.Push(7);
+            myStack.Push(9);
+            myStack.Push(6);
+
+            int lastObj = myStack.Pop(),
+                lastObjInMyStack = 6;
+
+            Assert.That(lastObj, Is.EqualTo(lastObjInMyStack));
+        }
+
+
+        [Test]
+        public void Pop_StackWithObjects_RemoveObjOnTheTop()
+        {
+            Stack<int> myStack = new Stack<int>();
+            myStack.Push(7);
+            myStack.Push(9);
+            myStack.Push(6);
+
+            int currentStackCount = myStack.Count,
+                lastObj = myStack.Pop(),
+                stackCountAfterPop = myStack.Count;
+
+            Assert.That(currentStackCount, Is.Not.EqualTo(stackCountAfterPop));
+        }
+
+
+        [Test]
+        public void Pop_EmptyStack_ShouldThrowInvalidOperationException()
+        {
+            Stack<object> myStack = new Stack<object>();
+
+            Assert.That(() => myStack.Pop(), Throws.InvalidOperationException);
+        }
+
+
+
+
 
 
 
